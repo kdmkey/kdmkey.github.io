44b0f6c226e2c6457821009a3b08b9522fdd434c
yuto kodama <yutokodama@kdm-2.local>
2026-03-09 20:35:50 +0900

    update

--- script.js
+++ script.js
@@ -9,3 +9,42 @@ window.addEventListener("scroll", function(){
     }
     
     });
+
+    /* ハンバーガーメニュー機能 */
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("menu");
+    const links = document.querySelectorAll(".menu a");
+    
+    /* ハンバーガークリック */
+    hamburger.addEventListener("click", function(e){
+    
+    e.stopPropagation(); // bodyクリック防止
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+    
+    });
+    
+    /* メニュークリックで閉じる */
+    links.forEach(function(link){
+    
+    link.addEventListener("click", function(){
+    
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+    
+    });
+    
+    });
+    
+    /* bodyクリックで閉じる */
+    document.body.addEventListener("click", function(){
+    
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+    
+    });
+    
+    /* メニュー内クリックは閉じない */
+    menu.addEventListener("click", function(e){
+    e.stopPropagation();
+    });
